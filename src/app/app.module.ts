cb380d18de7efe8abcdaa8d4ed4330f15150289e
Matias G P <mgabrielpalacio@gmail.com>
2021-08-02 20:31:50 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './menus/header/header.component';
+import { FooterComponent } from './menus/footer/footer.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IngresarDineroComponent } from './comp/ingresar-dinero/ingresar-dinero.component';
+import { EgresarDineroComponent } from './comp/egresar-dinero/egresar-dinero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    HomeComponent,
+    IngresarDineroComponent,
+    EgresarDineroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/comp/egresar-dinero/egresar-dinero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-egresar-dinero',
+  templateUrl: './egresar-dinero.component.html',
+  styleUrls: ['./egresar-dinero.component.css']
+})
+export class EgresarDineroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
