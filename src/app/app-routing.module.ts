f2ed8bb36ffc1094c4347e726848a909344f8523
Matias <mgabrielpalacio@gmail.com>
2021-08-08 18:22:22 -0300

    land3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './comp/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: LandingComponent},
+  // {path: 'Login', component: LoginComponent},
+  {path: 'movimientos', component:HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/comp/comp.module.ts
+++ src/app/comp/comp.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { IngresarDineroComponent } from './ingresar-dinero/ingresar-dinero.component';
+import { EgresarDineroComponent } from './egresar-dinero/egresar-dinero.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IngresarDineroComponent,
+    EgresarDineroComponent,
+    LoginComponent,
+    RegisterComponent
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule    
+  ],
+
+  exports: [
+    IngresarDineroComponent,
+    EgresarDineroComponent,
+    LoginComponent,
+    RegisterComponent
   ]
 })
 export class CompModule { }
