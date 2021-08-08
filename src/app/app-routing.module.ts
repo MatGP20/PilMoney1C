c574429e5cb31d0bfa87273f68c6738b829e4e7c
Matias <mgabrielpalacio@gmail.com>
2021-08-08 19:12:02 -0300

    land4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './comp/login/login.component';
+import { RegisterComponent } from './comp/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 
 
 const routes: Routes = [
   {path: 'home', component: LandingComponent},
-  // {path: 'Login', component: LoginComponent},
+  {path: 'Login', component: LoginComponent},
+  {path: 'Registro', component: RegisterComponent},
   {path: 'movimientos', component:HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   
