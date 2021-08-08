f7a3c3050084c772c9a2f94c52c16ea0a79988b0
Matias <mgabrielpalacio@gmail.com>
2021-08-08 20:20:59 -0300

    land5

--- src/app/comp/login/login.component.ts
+++ src/app/comp/login/login.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
+  mail = new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.emailPattern),
+  ]);
 
-  constructor() { }
+  passwordPattern =
+    '/^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&]{8,15}/';
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.passwordPattern),
+    Validators.minLength(8),
+  ]);
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+  get mailField() {
+    return this.mail;
   }
 
+  get passwordField() {
+    return this.password;
+  }
 }
