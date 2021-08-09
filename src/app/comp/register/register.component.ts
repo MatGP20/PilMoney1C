bf2013240bcfc0f3fc87cda6d461e3e35ab512bc
Pablo Andrés Ojeda <pabloaojeda@gmail.com>
2021-08-09 17:32:24 -0300

    validacion email y password formulario registro

--- src/app/comp/register/register.component.ts
+++ src/app/comp/register/register.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
+  mail = new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.emailPattern),
+  ]);
 
-  constructor() { }
+  passwordPattern =
+    '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,15}';
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.passwordPattern),
+    Validators.minLength(8),
+  ]);
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+  get mailField() {
+    return this.mail;
   }
 
+  get passwordField() {
+    return this.password;
+  }
 }
