69c81e3e2fb529f4da87930a48d0fc6807db6c76
Pablo Andrés Ojeda <pabloaojeda@gmail.com>
2021-08-09 00:07:03 -0300

    ingreso y egreso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,21 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './comp/login/login.component';
 import { RegisterComponent } from './comp/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { IngresarDineroComponent } from './comp/ingresar-dinero/ingresar-dinero.component';
+import { EgresarDineroComponent } from './comp/egresar-dinero/egresar-dinero.component';
 
 const routes: Routes = [
-  {path: 'home', component: LandingComponent},
-  {path: 'Login', component: LoginComponent},
-  {path: 'Registro', component: RegisterComponent},
-  {path: 'movimientos', component:HomeComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  
+  { path: 'home', component: LandingComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: 'Registro', component: RegisterComponent },
+  { path: 'movimientos', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'ingreso', component: IngresarDineroComponent },
+  { path: 'egreso', component: EgresarDineroComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
