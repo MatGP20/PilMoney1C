5423622c31fc6b7841f6a4e3258e818af5f587c7
Matias <mgabrielpalacio@gmail.com>
2021-09-11 19:16:57 -0300

    mas gestores

--- API/Controllers/LocalidadController.cs
+++ API/Controllers/LocalidadController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +9,7 @@ namespace API.Controllers
 {
     public class LocalidadController : ApiController
     {
+
+
     }
 }
